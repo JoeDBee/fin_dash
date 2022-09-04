041834144f46f33bce093a1fed422d2a53b3c455
JoeDBee <joe0brennan@gmail.com>
2022-09-04 12:49:18 +0100

    add additional drop down menu and pie chart components, add data schema

--- src/components/bar_chart.py
+++ src/components/bar_chart.py
@@ -1,26 +1,47 @@
+import pandas as pd
 import plotly.express as px
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 
-
 from . import ids
+from ..data.loader import DataSchema
 
 MEDAL_DATA = px.data.medals_long()
 
 
-def render(app: Dash) -> html.Div:
+def render(app: Dash, data: pd.DataFrame) -> html.Div:
     @app.callback(
         Output(ids.BAR_CHART, "children"),
-        Input(ids.NATION_DROPDOWN, "value")
+        [
+            Input(ids.YEAR_DROPDOWN, "value"),
+            Input(ids.MONTH_DROPDOWN, "value"),
+            Input(ids.CATEGORY_DROPDOWN, "value")
+        ]
     )
     # Note that this function is explicitly used anywhere. There's a bit of Python magic going on in the decorator,
     # which is perhaps not ideal. Useful for a fun application though :)
-    def update_bar_chart(nations: list[str]) -> html.Div:
-        filtered_data = MEDAL_DATA.query("nation in @nations") # similar to above comment, python magic with @nations
+    def update_bar_chart(years: list[str], months: list[str], categories: list[str]) -> html.Div:
+        # similar to above comment, python magic with @nations
+        filtered_data = data.query("year in @years and month in @months and category in @categories")
 
         if not filtered_data.shape[0]:
             return html.Div("No data selected :(")
-        fig = px.bar(filtered_data, x="medal", y="count", color="nation", text="nation")
+
+        def create_pivot_table() -> pd.DataFrame:
+            pt = filtered_data.pivot_table(
+                values=DataSchema.AMOUNT,
+                index=DataSchema.CATEGORY,
+                aggfunc="sum",
+                fill_value=0
+            )
+            return pt.reset_index().sort_values(DataSchema.AMOUNT, ascending=False)
+
+        fig = px.bar(
+            create_pivot_table(),
+            x=DataSchema.CATEGORY,
+            y=DataSchema.AMOUNT,
+            color=DataSchema.CATEGORY,
+        )
         return html.Div(dcc.Graph(figure=fig), id=ids.BAR_CHART)
 
     return html.Div(id=ids.BAR_CHART)
--- src/components/layout.py
+++ src/components/layout.py
@@ -1,8 +1,9 @@
+import pandas as pd
 from dash import Dash, html
-from src.components import dropdown, bar_chart
+from src.components import year_dropdown, month_dropdown, category_dropdown, bar_chart, pie_chart
 
 
-def create_layout(app: Dash) -> html.Div:
+def create_layout(app: Dash, data: pd.DataFrame) -> html.Div:
     return html.Div(
         className='app-dev',
         children=[
@@ -11,9 +12,12 @@ def create_layout(app: Dash) -> html.Div:
             html.Div(
                 className='dropdown-container',
                 children=[
-                    dropdown.render(app)
+                    year_dropdown.render(app, data),
+                    month_dropdown.render(app, data),
+                    category_dropdown.render(app, data)
                 ]
             ),
-            bar_chart.render(app)
+            bar_chart.render(app, data),
+            pie_chart.render(app, data)
         ],
     )
